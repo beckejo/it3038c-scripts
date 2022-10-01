7822cae6ad5076d4eba6164a43b6324b9f849f0b
beckejo <beckejo@mail.uc.edu>
2022-09-30 20:37:07 -0400

    Completed Lab 5

---
+++ python/myGetIP.py
@@ -0,0 +1,9 @@
+import socket, sys
+
+try:
+    hostname = str(sys.argv[1])
+    ip = socket.gethostbyname(hostname)
+    print (hostname +' has an IP of ' + ip)
+
+except:
+    print("Oops, something is wrong with that host")
