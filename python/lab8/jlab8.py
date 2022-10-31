44427f7f34088feb5eca5de0915dce3aafd7cfc7
beckejo <beckejo@mail.uc.edu>
2022-10-30 21:31:30 -0400

    complete lab 8

--- python/lab8/jlab8.py
+++ python/lab8/jlab8.py
@@ -1,10 +1,10 @@
 import requests, re
 from bs4 import BeautifulSoup
 
-data = requests.get("https://www.reebok.com/us/smiley-shaq-attaq-basketball-shoes/GX2250.html?cm_sp=SLOT-2.1-_-HOME_%3F_%3F_HOME_%3F-_-PRODUCTSELECTIONCAROUSEL-PRODUCT-CARD-_-957910").content 
+data = requests.get("https://www.reebok.com/us/smiley-club-c-85-mens-shoes/GV9492.html").content
 soup = BeautifulSoup(data, 'html.parser') 
-span = soup.find("h1", {"class":"product_information_title___2rG9M product_title gl-heading gl-heading--m"}) 
+span = soup.find("h1", {"class":"name___120FN"})
 title = span.text
-span = soup.find("span", {"class":"gl-price__value gl-price__value--sale"}) 
+span = soup.find("h1", {"class":"gl-price-item notranslate"})
 price = span.text
 print("Item %s has price %s" % (title, price))
