94b6a0140289adb18bfd0d55b0295026319aa057
beckejo <beckejo@mail.uc.edu>
2022-10-30 21:23:24 -0400

    lab 8

---
+++ python/Lab8/webscraping.py
@@ -0,0 +1,10 @@
+import requests, re
+from bs4 import BeautifulSoup
+
+data = requests.get("https://www.reebok.com/us/smiley-club-c-85-mens-shoes/GV9492.html").content
+soup = BeautifulSoup(data, 'html.parser')
+span = soup.find("span", {"class":"name___120FN"})
+title = span.text
+span = soup.find("span", {"class":"gl-price-item notranslate"})
+price = span.text
+print("Item %s has price %s" % (title, price))
